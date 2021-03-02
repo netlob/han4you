e3521e24cb1e86d8629f3caa9d8964f78f5d2997
idiidk <dviide@protonmail.com>
2021-03-02 13:47:39 +0100

    fix for importing from absolute path (1.0.1+1)

--- lib/providers/graphql-provider.dart
+++ lib/providers/graphql-provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:han4you/api/graphql/graphql.dart';
-import 'file:///C:/Users/idiidk/StudioProjects/han4you/lib/commons.dart';
+import 'package:han4you/commons.dart';
 
 class GraphQLProvider extends ChangeNotifier {
   GraphQL graphql = GraphQL(endpoint: Commons.graphQLEndpoint);
